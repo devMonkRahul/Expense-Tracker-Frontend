f4c31c88fda10ca76b497d51e40d988b56b1fae2
N8MARERahul <nightmarecoding0001@gmail.com>
2025-01-23 19:15:33 +0530

    refactor ErrorPopup to use lottie-react for improved animation handling

--- src/utils/Popup/ErrorPopup/ErrorPopup.jsx
+++ src/utils/Popup/ErrorPopup/ErrorPopup.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./ErrorPopup.css";
-import Lottie from "react-lottie";
+import Lottie from "lottie-react";
 import animationData from "../../../assets/Lottie/error.json";
 import { useSelector, useDispatch } from "react-redux";
 import { clearError } from "../../../store/features/errorSlice";
@@ -10,18 +10,10 @@ const ErrorPopup = () => {
   const dispatch = useDispatch();
   const { status, message } = useSelector((state) => state.error);
 
-  const defaultOptions = {
-    loop: false,
-    autoplay: true,
-    animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return status && (
     <div className=" fixed top-0 left-0 h-[100vh] w-[100vw] z-[10000] flex justify-center items-center bg-[#1414145e] backdrop-blur-md alertcontainer">
       <div className="alertcontent bg-[#ffffff]  rounded-md flex flex-col justify-center items-center px-[12%] py-10 gap-1">
-        <Lottie options={defaultOptions} height={200} width={200} />
+        <Lottie animationData={animationData} loop={false} height={200} width={200} />
         <p className="text-2xl font-semibold mb-8 text-black">{message}</p>
         <button
           onClick={() => dispatch(clearError())} 
